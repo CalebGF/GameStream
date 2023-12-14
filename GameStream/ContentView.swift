56b1076eb498fbe17a0417e33cbf3938bd8a27e4
Caleb <caleb2812@gmail.com>
2023-12-14 16:02:04 +0100

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Caleb Gonzalez on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Caleb Gonzalez on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
